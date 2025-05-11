ab49000800fe65e9ad4a3f3e0cf57ac3b57b85eb
mtvvi <matveyev.ilya@ya.ru>
2025-05-11 18:02:12 +0300

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,13 +26,32 @@ function App() {
         },
       });
       setResult(response.data);
-      setCurrentPage(1); // Сброс пагинации при новом запросе
+      setCurrentPage(1);
     } catch (error) {
       console.error('Error uploading file:', error);
     }
   };
 
-  // Рассчет данных для пагинации
+  const [showForm, setShowForm] = useState(false);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    try {
+      const response = await axios.post('http://localhost:8000/model/csv/predict', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setResult(response.data);
+      setCurrentPage(1);
+      setShowForm(false);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
+
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = result.slice(indexOfFirstItem, indexOfLastItem);
@@ -50,6 +69,44 @@ function App() {
           </div>
           <div>
             <button type="submit">Send to Prediction</button>
+            <button onClick={() => setShowForm(true)}>Add Single Record</button>
+
+            {showForm && (
+              <form onSubmit={handleFormSubmit} className="single-record-form">
+                <div>
+                  
+                </div>
+                <input type="text" name="user_id" placeholder="User ID" required />
+                <input type="text" name="nm_id" placeholder="NM ID" required />
+                <input type="datetime-local" step="1" name="CreatedDate" required />
+                <input type="text" name="service" placeholder="Service" required />
+                <input type="number" name="total_ordered" placeholder="Total Ordered" required />
+                <input type="text" name="PaymentType" placeholder="Payment Type" required />
+                
+                <input type="number" name="count_items" placeholder="Count Items" required />
+                <input type="number" name="unique_items" placeholder="Unique Items" required />
+                <input type="number" name="avg_unique_purchase" placeholder="Avg Unique Purchase" required />
+                <input type="number" name="NmAge" placeholder="NM Age" required />
+               
+                <input type="number" name="Distance" placeholder="Distance" required />
+                <input type="number" name="DaysAfterRegistration" placeholder="Days After Registration" required />
+                <input type="number" name="number_of_orders" placeholder="Number of Orders" required />
+                <input type="number" name="number_of_ordered_items" placeholder="Number of Ordered Items" required />
+                <input type="number" name="mean_number_of_ordered_items" placeholder="Mean Number of Ordered Items" required />
+                <input type="number" name="min_number_of_ordered_items" placeholder="Min Number of Ordered Items" required />
+                <input type="number" name="max_number_of_ordered_items" placeholder="Max Number of Ordered Items" required />
+                <input type="number" name="mean_percent_of_ordered_items" placeholder="Mean Percent of Ordered Items" required />
+                <input type="number" name="target" placeholder="Target" required />
+                
+                <div className="checkbox-container">
+                  <input type="checkbox" name="IsPaid" /> Is Paid
+                  <input type="checkbox" name="is_courier" /> Is Courier
+                </div>
+                
+                
+                <button type="submit">Submit Record</button>
+              </form>
+            )}
           </div>
         </form>
         
@@ -59,7 +116,7 @@ function App() {
             <table className="results-table">
               <thead>
                 <tr>
-                  <th>#</th>
+                  <th>№</th>
                   <th>Prediction</th>
                   <th>Confidence</th>
                 </tr>
@@ -68,8 +125,10 @@ function App() {
                 {currentItems.map((item, index) => (
                   <tr key={index}>
                     <td>{(currentPage - 1) * itemsPerPage + index + 1}</td>
-                    <td>{item.prediction ? 'Fraud' : 'Not Fraud'}</td>
-                    <td>{(item.confidence * 100).toFixed(2)}%</td>
+                    <td style={{ color: item.prediction ? 'red' : 'green' }}>
+                      {item.prediction ? 'Fraud' : 'Valid'}
+                    </td>
+                    <td>{(item.confidence * 100).toFixed(4)}%</td>
                   </tr>
                 ))}
               </tbody>
